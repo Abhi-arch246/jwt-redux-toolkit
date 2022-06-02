80b827c3bb31a3c4172b2cf9e5e99eabebbd15cd
Abhi-arch246 <abhishekreddy235@gmail.com>
2022-06-03 00:27:01 +0530

    Signup and login

---
+++ controllers/userController.js
@@ -0,0 +1,67 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/userModel')
+const bcrypt = require('bcryptjs/dist/bcrypt')
+
+
+// User register - /api/users
+const registerUser = async (req, res) => {
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+    if (userExists) {
+        return res.status(400).json({ msg: "Exists" })
+    } else {
+        try {
+            const salt = await bcrypt.genSalt(10)
+            const hashedPassword = await bcrypt.hash(password, salt)
+
+            const user = await User.create({
+                name,
+                email,
+                password: hashedPassword
+            })
+
+            res.status(201).json(user)
+
+        }
+        catch (error) {
+            console.log(error);
+            res.status(400).json({ msg: 'User not registered' })
+        }
+    }
+}
+
+// Login user - /api/users
+const loginUser = async (req, res) => {
+
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+
+        })
+    } else {
+        res.json({ message: "User not logged in" })
+
+    }
+}
+
+// User data - /api/users/me
+const dataUser = (req, res) => {
+    res.json({ message: "User data" })
+}
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' })
+}
+
+module.exports = {
+    registerUser,
+    loginUser,
+    dataUser
+}
--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-const express = require('require')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const userRoute = require('./routes/userRoute')
+const port = process.env.PORT || 5000
+const connectDb = require('./config/db')
+const app = express()
+
+app.use(express.json())
+
+connectDb()
+app.use("/api/users/", userRoute)
+
+app.listen(port, () => console.log(`Server running on ${port}`))
