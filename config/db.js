e09513780bbd71dac9e179aaa4ef3293f1bc452d
Abhi-arch246 <abhishekreddy235@gmail.com>
2022-06-04 20:13:36 +0530

    Deploy changes

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 
+const MONGO_URI = "mongodb+srv://abhiuser7:abhiuser7@cluster0.j00faur.mongodb.net/mern-jwt-auth?retryWrites=true&w=majority"
 const connectDb = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect(MONGO_URI)
         console.log(`Mongo server connected`);
     } catch (error) {
         console.log(error);
