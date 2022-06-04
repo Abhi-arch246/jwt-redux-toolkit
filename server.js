b8c42cdd6a03678ac40907e9223db65950932fda
Abhi-arch246 <abhishekreddy235@gmail.com>
2022-06-04 20:03:33 +0530

    Deploy changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const path = require('path')
 const userRoute = require('./routes/userRoute')
 const port = process.env.PORT || 5000
 const connectDb = require('./config/db')
@@ -9,5 +10,12 @@ app.use(express.json())
 
 connectDb()
 app.use("/api/users/", userRoute)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+    })
+}
 
 app.listen(port, () => console.log(`Server running on ${port}`))
